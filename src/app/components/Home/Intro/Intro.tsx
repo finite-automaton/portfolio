d5ff65e9491779be0196aa9410ec4d3f7ae6e7a1
finite-automaton <71351488+finite-automaton@users.noreply.github.com>
2024-01-29 21:38:35 +0000

    fix bio responsivity

--- src/app/components/Home/Intro/Intro.tsx
+++ src/app/components/Home/Intro/Intro.tsx
@@ -4,56 +4,57 @@ import Image from "next/image";
 import styles from "./intro.module.css";
 import profilePic from "../../../../../public/profile.png";
 import { useTranslation } from "@/app/hooks/useTranslation";
-import { useMediaQuery } from "@/app/hooks/useMediaQuery";
 
 export const Intro = () => {
   const { langDict } = useTranslation();
-  const isMobile = useMediaQuery("(max-width: 772px)");
 
   return (
     <section className={styles.section}>
-      {isMobile ? (
-        <div className={styles.profileWrapperMobile}>
-          <h1 className={styles.title}>{langDict.introduction.greeting}</h1>
-          <Image
-            className={styles.profilePictureMobile}
-            src={profilePic}
-            alt={"Picture of John Fletcher"}
-            priority
-          />
-          <div className={styles.textGroupMobile}>
-            <p className={styles.text}>{langDict.introduction.jobTitle1}</p>
-            <p className={styles.text}>{langDict.introduction.jobTitle2}</p>
-          </div>
+      {
+        // mobile
+      }
+      <div className={styles.profileWrapperMobile}>
+        <h1 className={styles.title}>{langDict.introduction.greeting}</h1>
+        <Image
+          className={styles.profilePictureMobile}
+          src={profilePic}
+          alt={"Picture of John Fletcher"}
+          priority
+        />
+        <div className={styles.textGroupMobile}>
+          <p className={styles.text}>{langDict.introduction.jobTitle1}</p>
+          <p className={styles.text}>{langDict.introduction.jobTitle2}</p>
         </div>
-      ) : (
-        <div className={styles.profileWrapperDesktop}>
-          <Image
-            className={styles.profilePictureDesktop}
-            src={profilePic}
-            alt={"Picture of John Fletcher"}
-            layout="responsive"
-            height={200}
-            width={200}
-            priority
-          />
-          <div className={styles.textGroupDesktop}>
-            <h1 className={styles.titleDesktop}>
-              {langDict.introduction.greeting}
-            </h1>
-            <p className={styles.text}>{langDict.introduction.jobTitle1}</p>
-            <p className={styles.text}>{langDict.introduction.jobTitle2}</p>
-          </div>
+      </div>
+      {
+        // desktop
+      }
+      <div className={styles.profileWrapperDesktop}>
+        <Image
+          className={styles.profilePictureDesktop}
+          src={profilePic}
+          alt={"Picture of John Fletcher"}
+          layout="responsive"
+          height={200}
+          width={200}
+          priority
+        />
+        <div className={styles.textGroupDesktop}>
+          <h1 className={styles.titleDesktop}>
+            {langDict.introduction.greeting}
+          </h1>
+          <p className={styles.text}>{langDict.introduction.jobTitle1}</p>
+          <p className={styles.text}>{langDict.introduction.jobTitle2}</p>
         </div>
-      )}
-      <Image
-        className={styles.chevron}
-        src="/chevron-down-solid.svg"
-        alt="down-facing-chevron"
-        height={50}
-        width={50}
-        priority
-      />
+        <Image
+          className={styles.chevron}
+          src="/chevron-down-solid.svg"
+          alt="down-facing-chevron"
+          height={50}
+          width={50}
+          priority
+        />
+      </div>
     </section>
   );
 };

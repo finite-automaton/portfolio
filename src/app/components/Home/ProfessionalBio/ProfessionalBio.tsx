2eafaed6b4350505d552f5123554e0707fc5c7b1
finite-automaton <71351488+finite-automaton@users.noreply.github.com>
2024-02-17 16:03:54 +0000

    add content

--- src/app/components/Home/ProfessionalBio/ProfessionalBio.tsx
+++ src/app/components/Home/ProfessionalBio/ProfessionalBio.tsx
@@ -1,8 +1,17 @@
 import { useContext } from "react";
+import Image from "next/image";
+
+import typescriptLogo from "../../../../../public/Typescript_logo_2020.svg";
+import reactLogo from "../../../../../public/React-icon.svg";
+import scalaLogo from "../../../../../public/scala-spiral.png";
+import pythonLogo from "../../../../../public/Python-logo-notext.svg";
 
 import { TranslationContext } from "../../Provider/Provider";
 
+import styles, { techLogoWrapper } from "./professionalBio.module.css";
 import homeStyles from "../home.module.css";
+
+const logos = [typescriptLogo, reactLogo, scalaLogo, pythonLogo];
 
 export const ProfessionalBio = () => {
   const { currentLangDict: langDict } = useContext(TranslationContext);
@@ -11,6 +20,13 @@ export const ProfessionalBio = () => {
       <h1 className={homeStyles.title}>My Professional Background</h1>
       <div className={homeStyles.textGroup}>
         <p className={homeStyles.text}>{langDict.bio.work}</p>
+      </div>
+      <div className={styles.techLogos}>
+        {logos.map((logoSrc) => (
+          <div className={techLogoWrapper} key={logoSrc}>
+            <Image className={styles.logo} src={logoSrc} fill alt="" />
+          </div>
+        ))}
       </div>
     </section>
   );

f449faac6252733a3b14c77ba56c402c3849a19a
John Fletcher <71351488+finite-automaton@users.noreply.github.com>
2024-02-17 16:04:48 +0000

    Add content (#16)
    * remove oscillation animation
    
    * remove priority prop
    
    * catch local storage error
    
    * add type guard
    
    * add content

--- src/app/components/Home/PersonalBio/PersonalBio.tsx
+++ src/app/components/Home/PersonalBio/PersonalBio.tsx
@@ -1,17 +1,25 @@
+import Image from "next/image";
+
+import hikingScotland from "../../../../../public/hiking_scotland.jpeg";
 import homeStyles from "../home.module.css";
 
+import styles from "./personalBio.module.css";
+import { useContext } from "react";
+import { TranslationContext } from "../../Provider/Provider";
+
 export const PersonalBio = () => {
+  const { currentLangDict: langDict } = useContext(TranslationContext);
+
   return (
     <section className={homeStyles.section}>
       <h1 className={homeStyles.title}>Interests</h1>
+
       <div className={homeStyles.textGroup}>
-        <p className={homeStyles.text}>
-          {" "}
-          Outside of work, I enjoy hiking, running and leanring languages.
-        </p>
-        <p className={homeStyles.text}>
-          I can speak advanced German and intermediate Fremch.
-        </p>
+        <p className={homeStyles.text}>{langDict.bio.personal1}</p>
+        <p className={homeStyles.text}>{langDict.bio.personal2} </p>
+      </div>
+      <div className={styles.imageWrapper}>
+        <Image className={styles.mainImage} src={hikingScotland} fill alt="" />
       </div>
     </section>
   );
